2a1be178a1a265baaa146d13038d6579732b51eb
Ethan Bickel <notethanbickel@gmail.com>
2025-10-17 21:23:35 -0500

    fix baseURL

--- src/env.mjs
+++ src/env.mjs
@@ -11,7 +11,7 @@ const server = z.object({
   BETTER_AUTH_URL: z.preprocess(
     // This makes Vercel deployments not fail if you don't set BETTER_AUTH_URL
     // Since Better Auth automatically uses the VERCEL_URL if present.
-    (str) => process.env.VERCEL_URL ?? str,
+    (str) => `https://${process.env.VERCEL_URL}` ?? str,
     // VERCEL_URL doesn't include `https` so it cant be validated as a URL
     process.env.VERCEL ? z.string().min(1) : z.string().url(),
   ),
