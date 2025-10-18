d92cd403e7446c9046c3b2e22b29346d80596007
Ethan Bickel <notethanbickel@gmail.com>
2025-10-17 21:17:14 -0500

    fix auth client

--- src/auth-client.ts
+++ src/auth-client.ts
@@ -1,5 +1,6 @@
 import { createAuthClient } from 'better-auth/react';
+import { env } from './env.mjs';
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: 'http://localhost:3000',
+  baseURL: env.BETTER_AUTH_URL,
 });
