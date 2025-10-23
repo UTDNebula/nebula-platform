c65bf2922f85bf68f314d05ef8d787052eaf3340
Ethan Bickel <notethanbickel@gmail.com>
2025-10-23 14:04:18 -0500

    testing vercel stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
         <h1 className="font-display text-5xl font-bold">NEBULA PLATFORM</h1>
         <h2 className="text-lg hero">All in One platform for all things nebula</h2>
         <GoogleButton />
+        <div>Vercel testing: {process.env.VERCEL_PROJECT_PRODUCTION_URL}</div>
       </div>
     </div>
   );
